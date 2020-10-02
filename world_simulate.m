c0acbe36b6e814fa1323d9dfef81f617e62d3eea
Bill Fan <1472451850@qq.com>
2020-10-02 10:52:01 -0400

    World: Added world simulate script

---
+++ world_simulate.m
@@ -0,0 +1,29 @@
+function [S, I, R, W] = world_simulate(...
+    s_0, i_0, r_0, ...
+    beta, gamma, alpha, lambda, ...
+    control, delay, num_steps)
+
+% Setup
+S = zeros(1, num_steps);
+I = zeros(1, num_steps);
+R = zeros(1, num_steps);
+W = 1 : num_steps;
+
+s = s_0;
+i = i_0;
+r = r_0;
+
+% Store initial values
+S(1) = s;
+I(1) = i;
+R(1) = r;
+
+% Run simulation
+for step = 2 : num_steps
+    [s, i, r] = model_step(s, i, r, beta, gamma, alpha, lambda, control);
+    S(step) = s;
+    I(step) = i;
+    R(step) = r;
+end
+
+end
